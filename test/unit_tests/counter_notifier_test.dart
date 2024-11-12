0486a1ca8da7822bf771d0f9ec7e6d5b339045bf
Haidar <haidarrmehsen@gmail.com>
2024-11-12 17:53:00 +0200

    Added starter test

---
+++ test/unit_tests/counter_notifier_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'CounterNotifier',
+    () {
+      test(
+        'initial value is 0',
+        () {
+          // TODO: Add your test code here
+        },
+      );
+    },
+  );
+}
